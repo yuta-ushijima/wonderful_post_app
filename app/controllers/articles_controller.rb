d1a71d97ee702175d7cd7acd9ac4c61cc678b8e9
ajk0421 <ajk.spirituallounge@gmai.com>
2021-12-12 03:26:06 +0000

    article index, show, new, create(Task7-3)

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show ]
+
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  def show
+  end
+
+  # GET /articles/new
+  def new
+    @article = Article.new
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: "Article was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'articles#index'
+  devise_for :users
   resources :articles
   resources :sample_articles
 end
