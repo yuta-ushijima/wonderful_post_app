a97ec8db22d3d5ba3a9a8c68d3fe085305b5e75e
ajk0421 <ajk.spirituallounge@gmai.com>
2021-12-12 02:54:15 +0000

    rails g model Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_015412) do
+ActiveRecord::Schema.define(version: 2021_12_12_025217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sample_articles", force: :cascade do |t|
     t.string "title"
