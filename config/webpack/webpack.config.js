3a9c6936019dcd44e294a1034b629bb4d20663df
Yuta_Ushizima <contact@yuta-u.com>
2022-11-13 13:09:50 +0900

    feat: Setting css/sass

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -2,18 +2,45 @@ const path    = require("path")
 const webpack = require("webpack")
 const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
 
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
 module.exports = {
+  entry: {
+    // add your css or sass entries
+    application: [
+      './app/javascript/application.js',
+      './app/javascript/stylesheets/application.scss',
+      './app/assets/stylesheets/application.css',
+    ],
+  },
+  module: {
+    rules: [
+      // Add CSS/SASS/SCSS rule with loaders
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  resolve: {
+    // Add additional file types
+    extensions: ['.js', '.jsx', '.scss', '.css'],
+  },
   mode: mode,
   devtool: "source-map",
-  entry: {
-    application: "./app/javascript/application.js"
-  },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "..", "..", "app/assets/builds"),
   },
   plugins: [
+    // Include plugins
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
     })
